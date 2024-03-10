d06f808077f99251f47cd380085687d5bb057537
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-03-11 01:25:04 +0500

    changes

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ let seconds = +prompt("Enter value2")
 document.write("Addition", firsts + seconds, "<br>");
 document.write("Subtraction", firsts - seconds, "<br>");
 document.write("Multiplication", firsts * seconds, "<br>");
-document.write("Divison", "", firsts / seconds, "<br>")
+document.write("Divison", firsts / seconds, "<br>")
